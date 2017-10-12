00ed5491be90d443bdbf6ee3c3d6dd4e64b888d8
Dante Cash <DanteCash98@gmail.com>
2017-10-12 17:41:02 -0500

    Fixed Constants

---
+++ app/src/main/java/com/bluecats/scratchingpost/Constants.java
@@ -0,0 +1,10 @@
+package com.bluecats.scratchingpost;
+
+import android.graphics.Color;
+
+public class Constants {
+    public static final String BLUECATS_APP_TOKEN = "";
+
+    public static final int[] ROW_COLOURS = new int[]{ Color.WHITE, Color.parseColor( "#EEEEEE" ) };
+
+}
